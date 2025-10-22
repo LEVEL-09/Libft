4422866b03ab91f7a2d7e71db0475baaab5512a0
Mohamed Khoubaz <hichamohicham651@gmail.com>
2025-10-22 18:42:56 +0100

    fix strlcpy

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mkhoubaz <mkhoubaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 07:10:20 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2025/10/19 07:57:58 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2025/10/22 18:41:50 by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-        char     *p;
-   
+        char    *p;
+
         p = (char *)malloc(sizeof(char) * len);
-        if (!p)      return(NULL);
+        if (!p)
+                return (NULL);
 
         ft_memcpy(p, s + start, len);
         return (p);
@@ -27,5 +28,5 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
 // int main()
 // {
 //     char str[] = "mkhoubaz@student";
-//     printf("%s \n", ft_substr(str, 8, 8)); 
+//     printf("%s \n", ft_substr(str, 8, 8));
 // }
