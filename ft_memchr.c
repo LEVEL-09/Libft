e96e4466da1aa99c747587db4c69fd9b810d2d64
Mohamed Khoubaz <hichamohicham651@gmail.com>
2025-10-24 20:48:21 +0100

    i need fix a lot :)

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,23 +6,24 @@
 /*   By: mkhoubaz <mkhoubaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 11:21:02 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2025/10/24 11:19:36 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2025/10/24 12:19:47 by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-    size_t      i;
-    unsigned char *ptr;
+	size_t			i;
+	unsigned char	*ptr;
 
-    ptr = (unsigned char*)s;
-    i = 0;
-    while (ptr[i] && i < n)
-    {
-        if (*(ptr + i) == (unsigned char)c)     return ((unsigned char*)s + i);
-        i++;
-    }
-    return (NULL); 
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (*(ptr + i) == (unsigned char)c)
+			return ((void *)s + i);
+		i++;
+	}
+	return (NULL);
 }
