32064a545ad78447c8c26babea7ecf12e761dfca
Mohamed Khoubaz <mkhoubaz@e3r5p2.1337.ma>
2025-10-24 11:57:30 +0100

    fix strnstr and memmove

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,21 +6,20 @@
 /*   By: mkhoubaz <mkhoubaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 08:43:28 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2025/10/23 08:58:18 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2025/10/24 11:54:14 by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-    size_t  i;
-    size_t  j;
-    size_t  find;
+    size_t	i;
+    size_t	j;
+    size_t	find;
 
     if (!(*little))
         return ((char *)big);
-
     i = 0;
     while (i < len && big[i])
     {
@@ -28,7 +27,7 @@ char *ft_strnstr(const char *big, const char *little, size_t len)
         if (big[i] == little[j])
         {
             find = i;
-            while (little[j])
+            while (i < len && little[j])
             {
                 if (big[i] != little[j])
                 {
@@ -46,6 +45,5 @@ char *ft_strnstr(const char *big, const char *little, size_t len)
         }
         i++;
     }
-
     return (NULL);
 }
